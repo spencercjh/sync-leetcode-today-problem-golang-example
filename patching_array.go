e936d7e2ffe14a2b9154b14ab3f1d5f2790357d7
spencercjh <shouspencercjh@foxmail.com>
2020-12-29 19:05:53 +0800

    330

--- patching_array.go
+++ patching_array.go
@@ -2,5 +2,15 @@ package leetcode
 
 // https://leetcode-cn.com/problems/patching-array
 func minPatches(nums []int, n int) int {
-
+	result := 0
+	for max, index := 1, 0; max <= n; {
+		if index < len(nums) && nums[index] <= max {
+			max += nums[index]
+			index++
+		} else {
+			max += max
+			result++
+		}
+	}
+	return result
 }
