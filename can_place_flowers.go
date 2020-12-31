73c9bcd3c228b18a93b83f045787e7a08ca2ce98
spencercjh <shouspencercjh@foxmail.com>
2021-01-01 03:12:45 +0800

    605

--- can_place_flowers.go
+++ can_place_flowers.go
@@ -2,5 +2,15 @@ package leetcode
 
 // https://leetcode-cn.com/problems/can-place-flowers
 func canPlaceFlowers(flowerbed []int, n int) bool {
-
+	for i := 0; i < len(flowerbed) && n > 0; {
+		if flowerbed[i] == 1 {
+			i += 2
+		} else if flowerbed[i] == 0 && (i+1 >= len(flowerbed) || flowerbed[i+1] == 0) {
+			i += 2
+			n--
+		} else {
+			i++
+		}
+	}
+	return n == 0
 }
